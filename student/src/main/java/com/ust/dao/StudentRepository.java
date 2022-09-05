49a5008cecec09a5aa8eb169b7d8fae692689296
203824 <203824@F052DTRV.ustr.com>
2022-09-05 15:58:15 +0530

    1st changes

---
+++ student/src/main/java/com/ust/dao/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.ust.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ust.model.Student; 
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer> { 
+} 
